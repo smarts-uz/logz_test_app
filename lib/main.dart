879c03fcff51a59285e6aabcf40e6a97776f704e
JasurbekErgashev <jergashev633@gmail.com>
2023-08-02 11:14:14 +0500

    added logz.io

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:logging/logging.dart';
+import 'package:logging_appenders/logging_appenders.dart';
+
+createLogger() {
+  Logger.root.level = Level.ALL;
+
+  LogzIoApiAppender(
+    apiToken: 'ToIujeviodyKLGBqKAcOCCiYbMebfjem',
+    url: 'https://listener.logz.io:8071',
+    labels: {'version': '1.0.0', 'build': '1'},
+  ).attachToLogger(Logger.root);
+}
 
 void main() {
+  createLogger();
   runApp(const MyApp());
 }
 
@@ -12,16 +25,40 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Logz.io Test',
       theme: ThemeData(useMaterial3: true),
-      home: const HomeScreen(),
+      home: HomeScreen(),
     );
   }
 }
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  final _logger = Logger('com.example.logz_test_app');
+  HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Logz.io'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: ElevatedButton(
+              child: const Text("Log '123' on Info"),
+              onPressed: () {
+                _logger.info("123");
+              },
+            ),
+          ),
+          ElevatedButton(
+            child: const Text("Log '456' on severe"),
+            onPressed: () {
+              _logger.severe("456");
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
